35c79a7385c157b8b1b50aa214c353ca58c3b263
Saad Guessous <guessous_saad@hotmail.com>
2013-09-15 22:46:39 +0300

    Installed PHPunit globally, practicing TDD ;) once again!

--- tests/PracticeTest.php
+++ tests/PracticeTest.php
@@ -1,18 +1,32 @@
 <?php
 
+require_once '/home/sguessou/composer-packages/vendor/phpunit/phpunit/PHPUnit/Framework/Assert/Functions.php';
+
+
 class PracticeTest extends PHPUnit_Framework_TestCase {
 	
 	public function testHelloWorld()
 	{
 		$greeting = 'Hello, World.';
-		$this->assertTrue($greeting === 'Hello, World.');
+		assertTrue($greeting === 'Hello, World.');
 	}
 
 	public function testAddition()
 	{
 		$sum = 2 + 2;
-		$this->assertEquals(4, $sum, 'Hello World!');
+		assertEquals(4, $sum, 'Hello World!');
 	}
+
+	public function testFamilyRequiresParent() {
+		
+		$family = [
+			'parents' => ['parents' => ['joe'], 'grandfather' => 'Heisenberg'],
+			'children' => ['Timmy', 'Suzy']
+			];
+		
+		assertArrayHasKey('grandfather', $family['parents']);
+	}
+
 
 
 }
