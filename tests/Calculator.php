706f188113027384468bc679f5623bbc9e6e6562
Saad Guessous <guessous_saad@hotmail.com>
2013-09-16 14:51:15 +0300

    Refactored Calculator class according to DRY principle.

--- tests/Calculator.php
+++ tests/Calculator.php
@@ -9,11 +9,42 @@ class Calculator {
 		return $this->result;
 	}
 
-	public function add($num)
+	public function add()
+	{
+		$this->calculateAll(func_get_args(), '+');	
+	}
+
+	public function subtract()
+	{
+		$this->calculateAll(func_get_args(), '-');
+	}
+
+	protected function calculateAll(array $nums, $symbol)
+	{
+		foreach ($nums as $num)
+		{
+			$this->calculate($num, $symbol);
+		}	
+	}
+
+	protected function calculate($num, $symbol)
 	{
 		if (! is_numeric($num))
 			throw new InvalidArgumentException;
 		
-		$this->result += $num;
+		switch($symbol)
+		{
+			case '+':
+				$this->result += $num;
+			break;
+			
+			case '-':
+				$this->result -= $num;
+			break;		
+		}
 	}
+
+	
+
+		
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,19 +2,14 @@
 
 class CalculatorTest extends PHPUnit_Framework_TestCase {
 
-	public function testResultDefaultToZero()
+	public function setup()
 	{
-		$calc = new Calculator;
-
-		$this->assertSame(0, $calc->getResult());
+		$this->calc = new Calculator;
 	}
 
-	public function testAddNumbers()
+	public function testResultDefaultToZero()
 	{
-		$calc = new Calculator;
-		$calc->add(5);
-
-		$this->assertEquals(5, $calc->getResult());
+		$this->assertSame(0, $this->calc->getResult());
 	}
 
    
@@ -23,8 +18,31 @@ class CalculatorTest extends PHPUnit_Framework_TestCase {
 	*/
 	public function testRequireNumericValue()
 	{
-		$calc = new Calculator;
-		$calc->add('five');
+		$this->calc->add('five');
+	}
+
+
+	public function testAcceptsMultipleArgs()
+	{
+		$this->calc->add(1, 2, 3, 4);
+
+		$this->assertEquals(10, $this->calc->getResult());
+		$this->assertNotEquals('Snoop Doggy Dogg and Dr. Dre is at the door', $this->calc->getResult());
+
+	}
+
+	public function testAddNumbers()
+	{
+		$this->calc->add(5);
+
+		$this->assertEquals(5, $this->calc->getResult());
+	}
+
+	public function testSubtract()
+	{
+		$this->calc->subtract(4);
+
+		$this->assertEquals(-4, $this->calc->getResult());
 	}
 
 }
